9df474207c2cfb2872bc26044b1452f4a11cc810
Atharv <atharvpatil5177@gmail.com>
2023-03-07 02:50:49 +0530

    side navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
+import { Fade } from "react-awesome-reveal";
 
 function Header() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleComponent = () => {
+    setIsVisible(!isVisible);
+  };
   return (
     <NavContainer>
       <Img src="/images/logo.svg" alt="tesla-logo" />
@@ -16,8 +23,29 @@ function Header() {
       <NavMenuRight>
         <a href="#">Shop</a>
         <a href="#">Account</a>
-        <a href="#">Menu</a>
+        <button onClick={toggleComponent}>Menu</button>
       </NavMenuRight>
+      <MobileMenu>
+        <button onClick={toggleComponent}>Menu</button>
+      </MobileMenu>
+      <Sidenav visible={isVisible}>
+        <Fade direction="right" triggerOnce="true">
+          <ul>
+            <li>Model S</li>
+            <li>Model 3</li>
+            <li>Model X</li>
+            <li>Model Y</li>
+            <li>Solar Roof</li>
+            <li>Solar Panels</li>
+            <li>Existing Inventory</li>
+            <li>Used Inventory</li>
+            <li>Trade-In</li>
+            <li>Demo Drive</li>
+            <li>Insurance</li>
+            <li>Powerwall</li>
+          </ul>
+        </Fade>
+      </Sidenav>
     </NavContainer>
   );
 }
@@ -55,10 +83,69 @@ const NavMenu = styled.div`
 `;
 
 const NavMenuRight = styled(NavMenu)`
+  button {
+    margin-right: 15px;
+    border: 1px solid black;
+    background-color: transparent;
+    padding: 5px;
+    border-radius: 3px;
+    color: black;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 550;
+    backdrop-filter: blur(5px);
+    z-index: 3;
+  }
+`;
+
+const MobileMenu = styled.div`
+  display: none;
   @media only screen and (max-width: 1120px) {
-    ${
-      "" /* display: flex;
-    align-items: center; */
+    display: flex;
+    width: 90px;
+    align-items: center;
+    justify-content: center;
+    button {
+      border: 1px solid black;
+      background-color: transparent;
+      padding: 5px;
+      border-radius: 3px;
+      color: black;
+      cursor: pointer;
+      font-size: 14px;
+      font-weight: 550;
+      backdrop-filter: blur(5px);
+      z-index: 3;
+    }
+  }
+`;
+
+const Sidenav = styled.div`
+  display: ${(props) => (props.visible ? "block" : "none")};
+  background-color: #f9f9f9;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  height: 100vh;
+  width: 200px;
+  @media only screen and (min-width: 1120px) {
+    width: 300px;
+  }
+  z-index: 2;
+  transition: 0.2s all ease-in;
+  ul {
+    margin-top: 50px;
+    margin-left: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  li {
+    list-style: none;
+    margin-top: 25px;
+    &:hover {
+      cursor: pointer;
     }
   }
 `;
